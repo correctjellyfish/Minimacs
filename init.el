896c27cbac58e8af411f2ce325b52ef0451f23e3
Braden Griebel <bgriebel@uw.edu>
2026-01-13 20:44:22 -0800

    Selected package update

--- init.el
+++ init.el
@@ -233,8 +233,15 @@ If the new path's directories does not exist, create them."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil)
- )
+ '(package-selected-packages
+   '(cape catppuccin-theme centaur-tabs change-inner corfu-terminal
+	  dap-mode dashboard eat embark-consult ess flycheck-pos-tip
+	  format-all general json-mode kind-icon lsp-ui magit
+	  marginalia multiple-cursors orderless rust-mode tempel
+	  typst-ts-mode vertico wgrep yaml-mode))
+ '(package-vc-selected-packages
+   '((typst-ts-mode :url
+		    "https://codeberg.org/meow_king/typst-ts-mode.git"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
