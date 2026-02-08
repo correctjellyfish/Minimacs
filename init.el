b6231681cf91cdec83fec254fcb21ad90c556313
Braden Griebel <bgriebel@uw.edu>
2026-02-07 22:40:53 -0800

    Additional ocaml setup

--- init.el
+++ init.el
@@ -753,9 +753,33 @@ If the new path's directories does not exist, create them."
   )
 (load "ess-autoloads")
 
+;; Major mode for OCaml programming
 (use-package tuareg
+  :ensure t
+  :mode (("\\.ocamlinit\\'" . tuareg-mode)))
+
+;; Major mode for editing Dune project files
+(use-package dune
   :ensure t)
 
+;; Merlin provides advanced IDE features
+(use-package merlin
+  :ensure t
+  :config
+  (add-hook 'tuareg-mode-hook #'merlin-mode)
+  (add-hook 'merlin-mode-hook #'company-mode)
+  ;; we're using flycheck instead
+  (setq merlin-error-after-save nil))
+
+(use-package merlin-eldoc
+  :ensure t
+  :hook ((tuareg-mode) . merlin-eldoc-setup))
+
+;; This uses Merlin internally
+(use-package flycheck-ocaml
+  :ensure t
+  :config
+  (flycheck-ocaml-setup))
 
 ;;;;;;;;;;;;;;;;;;;
 ;;;  Templating  ;;
